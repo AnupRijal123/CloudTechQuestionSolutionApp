67ffc3608141ba0013061586a1ed98cce3caa6e5
Anup <anuprijal321@gmail.com>
2024-07-08 20:58:51 +0545

    first deploy done

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0tysOCob_tzQV4sxYxPWvtmoOUYaOqsI",
+    authDomain: "cloudtech-question-solve.firebaseapp.com",
+    projectId: "cloudtech-question-solve",
+    storageBucket: "cloudtech-question-solve.appspot.com",
+    messagingSenderId: "626923559193",
+    appId: "1:626923559193:web:089d3c3562c1673c420d88"
+};
+
+const app = initializeApp(firebaseConfig);
